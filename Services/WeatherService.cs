fa0da42e33ad83e10f4018f80c60821dc1ce4ece
dilanmelvin <dilan4524melvin@gmail>
2024-10-11 12:00:52 +0530

    5 days forcast

--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -2,19 +2,22 @@ namespace LiveWeather.Services;
 
 using Microsoft.Extensions.Options;
 using LiveWeather.Models;
+
 public class WeatherService
 {
     private readonly HttpClient _httpClient;
- private readonly WeatherApiOptions _options;
-    public WeatherService(HttpClient httpClient,IOptions<WeatherApiOptions> options){
+    private readonly WeatherApiOptions _options;
+
+    public WeatherService(HttpClient httpClient, IOptions<WeatherApiOptions> options)
+    {
         _httpClient = httpClient;
         _options = options.Value;
-
     }
-    public async Task<WeatherResponse> GetWeatherAsync(string city)
+
+    public async Task<WeatherResponse> Get5DayForecastAsync(string city)
     {
-      
-        var weather = await _httpClient.GetFromJsonAsync<WeatherResponse>($"?key={_options.ApiKey}&q={city}");
-        return weather;
+        var forecast = await _httpClient.GetFromJsonAsync<WeatherResponse>(
+            $"{_options.BaseUrl}/forecast.json?key={_options.ApiKey}&q={city}&days=5");
+        return forecast;
     }
 }
